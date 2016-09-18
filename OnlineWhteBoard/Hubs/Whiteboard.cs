c95ee98f351b474e072f38ac735a23cef2c869f0
Digvijay Singh <diggudg@gmail.com>
2016-09-18 14:05:47 +0530

    1st

---
+++ OnlineWhteBoard/Hubs/Whiteboard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace OnlineWhteBoard.Hubs
+{
+    [HubName("whiteBoard")]
+    public class Whiteboard : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+        public void UpdateCanvas(int x, int y, int a, int b)
+        {
+            Clients.All.createCanvas(x, y, a, b);
+        }
+        public void EraseCanvas(int x, int y, int a, int b)
+        {
+
+        }
+    }
+}
